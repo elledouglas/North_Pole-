b14539037464904877e1b81b910983f4014f5f99
Kathleen <kathleenmcclay@gmail.com>
2017-07-12 20:44:57 -0400

    Added methods to naughty_letter

--- naughty_letter_writer.rb
+++ naughty_letter_writer.rb
@@ -11,6 +11,9 @@ kids_data.each_line do |kid|
   gender   = kid_data_array[1]
   behavior = kid_data_array[2]
   toys     = kid_data_array[3..8]
+  toy = toys.sample
+  infraction = kid_data_array[15..kid_data_array.length-1].to_a.join(' ')
+  naughty_toys = toys.reject {|t| toy == t}.join(', ')
 
   next unless behavior == 'naughty'
 
